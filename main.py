2a82c77a7e995726704b9d7f98fc3e5913caa57b
Maxim <naslednik8120@gmail.com>
2022-11-24 20:58:06 +0300

    Max number of start matrix

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ matrix = np.matrix([[np.nan, np.nan, 9, 1],
 print(matrix)
 print(np.shape(matrix)[0])
 users_count = np.shape(matrix)[0]
-print(type(users_count))
-hidden_factors = int(input())
+print(type(np.nan))
 
-U_matrix = np.matrix([])
+def maxnum(matrix_op):
+    numbers = []
+    for i in range(np.shape(matrix_op)[0]):
+        for j in range(np.shape(matrix_op)[1]):
+            if not np.isnan(matrix_op[i, j]):
+                numbers.append(matrix_op[i, j])
+    print(numbers)
+    return max(numbers)
+
+
+max_number = maxnum(matrix)
+hidden_factors = int(input())
+U_matrix = np.round(max_number * np.random.random_sample((np.shape(matrix)[0], hidden_factors)), 2)
 print(U_matrix)
 
-V_matrix = np.round(5 * np.random.random_sample((users_count, hidden_factors)), 2)
+V_matrix = np.round(max_number * np.random.random_sample((hidden_factors, np.shape(matrix)[1])), 2)
 print(V_matrix)
+

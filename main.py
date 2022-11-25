580a9c6b15d11b7d736d51fc85294e6b0ae3946f
RGBAF <maxkire111@gmail.com>
2022-11-25 13:18:10 +0300

    Return indexes of only numbers

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ matrix = np.matrix([[np.nan, np.nan, 9, 1],
                     [3, np.nan, 7, np.nan],
                     [5, np.nan, np.nan, 10],
                     [np.nan, 2, np.nan, np.nan]])
+
+
 print(matrix)
 print(np.shape(matrix)[0])
-users_count = np.shape(matrix)[0]
-print(type(np.nan))
+
+
 
 def maxnum(matrix_op):
     return max([j for i in matrix_op.tolist() for j in i if not np.isnan(j)])
 
+def indexnums(matrix_op):
+    return [(i, j) for i in range(len(matrix_op.tolist())) for j in range(len(matrix_op.tolist()[i])) if not np.isnan(matrix_op.tolist()[i][j])]
+
 max_number = maxnum(matrix)
-print(max_number)
+index_numbers = indexnums(matrix)
 hidden_factors = int(input())
 U_matrix = np.round(max_number * np.random.random_sample((np.shape(matrix)[0], hidden_factors)), 2)
 print(U_matrix)

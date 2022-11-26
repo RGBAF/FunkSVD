2d08025e133767131abee44d7e8ae9807a355f81
Xlatalk <xlatentalk@gmail.com>
2022-11-27 00:44:39 +0300

    Pre-release(mode-1 not function)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def test_matrix():
     V = np.matrix([[-1.8, 1, -0.2, 1], [0.5, 1.2, 0.1, 5], [1.4, 4, 0.14, 2]])
     return U, V
 
+
 def generate_matrix(h_f, m_n):
     U = np.round(m_n * np.random.random_sample((np.shape(matrix)[0], h_f)), 2)
     V = np.round(m_n * np.random.random_sample((h_f, np.shape(matrix)[1])), 2)
@@ -28,7 +29,7 @@ def indexnums(matrix_op):
 
 
 def quad_err(x_actual, x_predicted):
-    return (x_actual - x_predicted)**2
+    return ((x_actual - x_predicted)**2)/2
 
 
 def predict(row, column, U, V):
@@ -41,35 +42,58 @@ def predict(row, column, U, V):
 def factor_values(x_row, y_row, x_actual, x_predicted):
     value_list = []
     for i in range(np.shape(x_row)[1]):
-        value_list.append(round(x_row[0, i] + 0.1 * 2 * (x_actual - x_predicted) * y_row[0, i], 2))
+        value_list.append(round(x_row[0, i] + 0.05 * 2 * (x_actual - x_predicted) * y_row[0, i], 2))
     return value_list
 
-def assemble_U(row_i, U_matrix, factor_U):
-    return 0
-def assemble_V(row_i, V_matrix, factor_V):
-    V_matrix = np.transpose(V_matrix)
-    return np.transpose(V_matrix)
+
+def assemble_U(row_i, U_fuck, factor_U):
+    U_fuck[row_i] = factor_U
+    return U_fuck
+
+
+def assemble_V(row_i, V_fact, factor_V):
+    np.transpose(V_fact)[row_i] = factor_V
+    return V_fact
 
 
 def Iteration(index, matrix_op, U, V):
     x_act = matrix_op[index[0], index[1]]
     x_predict = round(predict(index[0], index[1], U, V), 2)
-    error = round(quad_err(x_act, x_predict), 2)
-    V_row = factor_values(U[index[0]], np.transpose(V)[index[1]], x_act, x_predict)
-    U_row = factor_values(np.transpose(V)[index[1]], U[index[0]], x_act, x_predict)
-    return V_row, U_row, error
+    err = round(quad_err(x_act, x_predict), 2)
+    U_row = factor_values(U[index[0]], np.transpose(V)[index[1]], x_act, x_predict)
+    V_row = factor_values(np.transpose(V)[index[1]], U[index[0]], x_act, x_predict)
+    print(f'Фактическое значение: {x_act}')
+    print(f'Рассчитанное значение: {x_predict}')
+    print(f'Квадратичная ошибка: {err}')
+    print(f'Обновленные значения скрытых факторов пользователя : {U_row}')
+    print(f'Обновленные значения скрытых факторов объектов : {V_row}')
+    return U_row, V_row, err
 
 
+def replace_nan(matrix_op, U, V):
+    nan_list = [(i, j)
+                for i in range(len(matrix_op.tolist()))
+                for j in range(len(matrix_op.tolist()[i]))
+                if np.isnan(matrix_op.tolist()[i][j])]
+    for el in nan_list:
+        matrix_op[el[0], el[1]] = round(predict(el[0], el[1], U, V), 2)
+    return matrix_op
 
 
-
+def replace_all(matrix_op, U, V):
+    all_list = [(i, j)
+                for i in range(len(matrix_op.tolist()))
+                for j in range(len(matrix_op.tolist()[i]))]
+    for el in all_list:
+        matrix_op[el[0], el[1]] = round(predict(el[0], el[1], U, V), 2)
+    return matrix_op
 
 
 print(f'Исходная матрица:\n{matrix}')
 
 while True:
-    # mode = int(input('Выберите режим:\n0 - для теста по методичке;\n1 - для штатной работы;\n'))
-    mode = 0
+    mode = int(input('Выберите режим:\n0 - для теста по методичке;\n1 - для штатной работы;\n'))
+    # mode = 0
     if mode == 0:
         U_matrix, V_matrix = test_matrix()
         break
@@ -82,27 +106,48 @@ while True:
     else:
         print('!!!Введены некорректные значения!!!')
 
-EPS = 100
+EPS = 0.5
 index_numbers = indexnums(matrix)
-print(index_numbers)
-count = 1
-for i in index_numbers:
-    while True:
-        print(i)
-        print(f'_______ИТЕРАЦИЯ № {count}_______')
-        f_V, f_U, error = Iteration(i, matrix, U_matrix, V_matrix)
-        print(f_V)
-        print(f_U)
-        print(error)
-        count += 1
-        if error <= EPS:
-            break
-
-
 print(f"U matrix:\n{U_matrix}")
 print()
-print(f"V.T matrix:\n{V_matrix}")
-print(f"e = {round(predict(index_numbers[0][0],index_numbers[0][1], U_matrix, V_matrix), 2)}")
-print(f"Error = {round(quad_err(matrix[index_numbers[0][0], index_numbers[0][1]],round(predict(index_numbers[0][0],index_numbers[0][1], U_matrix, V_matrix), 2)),2)}")
-print(Iteration(index_numbers[0], matrix, U_matrix, V_matrix))
-print()
+print(f"Vt matrix:\n{V_matrix}")
+count = 1
+check = [index_numbers[0]]
+for element in index_numbers:
+    print(f'########## ЭЛЕМЕНТ : {matrix[element[0], element[1]]} ##########')
+    while True:
+        print(f'_______Итерация № {count}_______')
+        f_U, f_V, error = Iteration(element, matrix, U_matrix, V_matrix)
+        for chi in check:
+            if chi == element:
+                case = 0
+                break
+            elif chi[1] == element[1]:
+                case = 1
+                break
+            elif chi[0] == element[0]:
+                case = 2
+                break
+            else:
+                case = 0
+                break
+        if case == 0:
+            U_matrix = assemble_U(element[0], U_matrix, f_U)
+            V_matrix = assemble_V(element[1], V_matrix, f_V)
+        elif case == 1:
+            print('Салам 2')
+            U_matrix = assemble_U(element[0], U_matrix, f_U)
+        elif case == 2:
+            print('Салам 3')
+            V_matrix = assemble_V(element[1], V_matrix, f_V)
+        case = -1
+        print(f"U matrix:\n{U_matrix}")
+        print()
+        print(f"Vt matrix:\n{V_matrix}")
+        count += 1
+        if error <= EPS:
+            count = 1
+            check.append(element)
+            break
+print(f'Заполненная матрица:\n{replace_nan(matrix, U_matrix, V_matrix)}')
+print(f'Заполненная матрица ПОЛНОСТЬЮ:\n{replace_all(matrix, U_matrix, V_matrix)}')
